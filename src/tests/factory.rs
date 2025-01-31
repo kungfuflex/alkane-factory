09f8508ee2e6da64eb2d0d7627c9e2767a50f438
kungfuflex <judoflexchop@gmail.com>
2025-01-31 21:24:03 +0000

    error

--- src/tests/factory.rs
+++ src/tests/factory.rs
@@ -1,8 +1,11 @@
 use alkanes::message::AlkaneMessageContext;
 use alkanes::view;
+use alkanes::precompiled::{alkanes_std_auth_token_build};
 use alkanes_support::gz::compress;
 use alkanes_support::id::AlkaneId;
+use alkanes_support::constants::{AUTH_TOKEN_FACTORY_ID};
 use alkanes_support::trace::Trace;
+use alkane_factory_support::constants::{ALKANE_FACTORY_OWNED_TOKEN_ID};
 use anyhow::Result;
 use bitcoin::address::NetworkChecked;
 use bitcoin::blockdata::transaction::OutPoint;
@@ -22,7 +25,7 @@ use protorune_support::protostone::Protostone;
 
 use protorune_support::utils::consensus_encode;
 
-use crate::tests::std::free_mint_build;
+use crate::tests::std::{free_mint_build, owned_token_build};
 use crate::tests::player1_build;
 use alkane_factory_support::constants::ALKANE_FACTORY_FREE_MINT_ID;
 use alkane_helpers::clear;
@@ -102,5 +105,87 @@ fn test_factory() -> Result<()> {
     println!("{:?}", callresponse_data);
     println!("gzipped: {:?}", returnable_data);
     assert!(callresponse_data == returnable_data);
+    Ok(())
+}
+
+#[wasm_bindgen_test]
+fn test_owned_token() -> Result<()> {
+    clear();
+    set_view_mode();
+    let mut block_height = 850_000;
+    let cellpacks: Vec<Cellpack> = [
+        //auth token factory init
+        Cellpack {
+            target: AlkaneId {
+                block: 3,
+                tx: AUTH_TOKEN_FACTORY_ID
+            },
+            inputs: vec![100]
+        },
+        Cellpack {
+            target: AlkaneId {
+                block: 3,
+                tx: ALKANE_FACTORY_OWNED_TOKEN_ID
+            },
+            inputs: vec![100],
+        }
+    ].into();
+    let next: Vec<Cellpack> = [Cellpack {
+            target: AlkaneId {
+                block: 6,
+                tx: ALKANE_FACTORY_OWNED_TOKEN_ID
+            },
+            inputs: vec![0, 1, 0, 0x414243454748, 0x414243454748]
+        }].into();
+    let returnable_data = player1_build::get_bytes();
+    let mut test_block_last = alkane_helpers::init_with_multiple_cellpacks_with_tx(
+        [alkanes_std_auth_token_build::get_bytes(), owned_token_build::get_bytes()].into(),
+        cellpacks,
+    );
+    index_block(&test_block_last, block_height - 1)?;
+    let mut test_block = alkane_helpers::init_with_multiple_cellpacks_with_tx(
+        [vec![]].into(),
+        next,
+    );
+    let mut output: &mut TxOut  = {
+      let len = test_block.txdata.len();
+      let mut tx = &mut test_block.txdata[len - 1];
+      let out_len = tx.output.len();
+      &mut tx.output[out_len - 1]
+    };
+//    output.script_pubkey = Script::from_bytes(&hex_lit::hex!("6a5d24ff7f8190ec82d08bc0a886a982848c9fa1fd8301ff7fa0b7eac4cfd5add6819f87888573")).into();
+    //println!("{:?}", test_block.txdata[test_block.txdata.len() - 1]);
+    let len = test_block.txdata.len();
+    let outpoint = OutPoint {
+        txid: test_block.txdata[len - 1].compute_txid(),
+        vout: 0,
+    };
+    index_block(&test_block, block_height)?;
+    let ptr = RuneTable::for_protocol(AlkaneMessageContext::protocol_tag())
+        .OUTPOINT_TO_RUNES
+        .select(&consensus_encode(&outpoint)?);
+    let sheet = load_sheet(&ptr);
+/*
+    println!("balances at end {:?}", sheet);
+    println!(
+        "{:?}",
+        <Vec<u8> as TryInto<Trace>>::try_into(view::trace(&OutPoint {
+            txid: test_block.txdata[len - 1].compute_txid(),
+            vout: 3
+        })?)?
+    );
+*/
+    let mut parcel = MessageContextParcel::default();
+    parcel.height = 850001;
+    parcel.calldata = (Cellpack {
+        target: AlkaneId { block: 2, tx: 1 },
+        inputs: vec![1000u128],
+    })
+    .encipher();
+ //   println!("calldata: {:?}", &parcel.calldata);
+    let callresponse_data = view::simulate_parcel(&parcel, u64::MAX)?.0.data;
+ //   println!("{:?}", callresponse_data);
+    println!("gzipped: {:?}", returnable_data);
+//    assert!(callresponse_data == returnable_data);
     Ok(())
 }
